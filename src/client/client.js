28afd50b6727022b97b2c669c4879de0ad4eae96
hydrabolt <amishshah.2k@gmail.com>
2016-03-04 17:10:10 +0000

    debug

--- src/client/client.js
+++ src/client/client.js
@@ -20,6 +20,8 @@ class LucidClient extends EventEmitter{
 		this.api
 			._get("/meta")
 			.end((err, res) => {
+				alert(err);
+				alert(res.body.message);
 				if(err){
 					callback(err);
 					try{
